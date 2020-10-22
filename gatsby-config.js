20636f05b98fe06ee5fee553f4907b76b2469b81
Nick <nickeast1998@gmail.com>
2020-10-22 14:42:58 +0100

    dotenv gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env' });
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
+console.log(`Using environment config: '${activeEnv}'`);
+require('dotenv').config({
+  path: `.env.${activeEnv}`,
+});
 
 export default {
   siteMetadata: {
