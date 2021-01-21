00c6bc191e7bcf120dc3ccb7532d62a742ca8526
Nick <nickeast1998@gmail.com>
2021-01-21 11:19:36 +0000

    gatsby manifest icon changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: `#162129`,
         theme_color: `#1bbc9b`,
         display: `minimal-ui`,
-        icon: `./src/assets/images/icon.png`,
+        icon: `/icon.png`,
       },
     },
     {
